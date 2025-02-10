9ecab91dfea75b4e553a99de9b56158f7c7a307f
kiko <sjy908159199@126.com>
2025-02-10 20:32:47 +0800

    add redis test

---
+++ src/test/java/cn/kiko/net_monitor_analysis_system/redis/RedisTest.java
@@ -0,0 +1,43 @@
+package cn.kiko.net_monitor_analysis_system.redis;
+
+import cn.kiko.net_monitor_analysis_system.runner.ServerStarter;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.concurrent.CountDownLatch;
+
+@SpringBootTest
+public class RedisTest {
+    // 不要实例化这个 bean，否则会启动服务阻塞测试
+    @MockBean
+    private ServerStarter starter;
+
+    private static final Logger logger = LoggerFactory.getLogger(RedisTest.class);
+
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+
+    private final String testKey = "test:key1";
+    @Test
+    public void redisTest() throws InterruptedException {
+        int count = 5;
+        CountDownLatch latch = new CountDownLatch(count);
+        for (int i = 0; i < count; ++i) {
+            new Thread(() -> {
+                for (int j = 0; j < 5000; ++j) {
+                    redisTemplate.opsForValue().increment(testKey, 1);
+                }
+                latch.countDown();
+            }).start();
+        }
+        latch.await();
+        logger.info("get key, result: {}", redisTemplate.opsForValue().get(testKey));
+        logger.info("deleted: {}", redisTemplate.delete(testKey));
+        logger.info("deleted: {}", redisTemplate.delete(testKey));
+    }
+}
